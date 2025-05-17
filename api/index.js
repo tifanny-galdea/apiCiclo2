95c16da94028785e6081e86b625c56e5f27f4267
tifanny-galdea <tifannygm13@gmail.com>
2025-05-17 14:44:27 -0500

    API -Token3

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
 
-import app from '../src/app.js';
+import app from './src/app.js';
 
-export default function handler(req, res) {
-  app(req, res); // Vercel ejecuta Express como handler
-}
+export default app;
